6ffaa3c05a89e1ade36025ab32065c15561f89f2
revanthkumaar <iamrevanthkumar@gmail.com>
2021-10-12 15:21:47 +0530

    asdsdfs
    qads

--- practice/all react projects/restraunt-app/src/child-components/single-restraunt-component/singlerestraunt.component.js
+++ practice/all react projects/restraunt-app/src/child-components/single-restraunt-component/singlerestraunt.component.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SingleRestraunt = ({listofplaces}) => {
-  console.log('this is inside single restraunt');
+const SingleRestraunt = ({ listofplaces }) => {
+  console.log("this is inside single restraunt");
   console.log(listofplaces);
   return (
     <div className="single-restraunt-component">
       <p>this is single restraunt component</p>
-      <p>the name of the second restraunt is {listofplaces[1].name}</p>
+      {listofplaces.map((place) => (
+        <p>{place.name}</p>
+      ))}
     </div>
   );
 };
