c8131073eb37e471685a743fdd237710e952c14a
revanthkumaar <iamrevanthkumar@gmail.com>
2021-10-18 15:44:01 +0530

    aqsdas
    afdsasd

--- practice/all react projects/restraunt-app/src/App.js
+++ practice/all react projects/restraunt-app/src/App.js
@@ -4,14 +4,17 @@ import Booking from './pages/booking-component/booking.component'
 import RestrauntList from './pages/restraunt-list-component/restraunt.list.component'
 import EventHandlerExample from './child-components/eventHandlerWithReact/eventHandlerExample.jsx'
 import EventHandleExampleWithClass from './child-components/eventHandlerWithReact/eventHandleExampWithClassComp'
+import BasicRoutingExample from './pages/BasicRoutingExample'
 function App() {
   return (
     <div className="App">
-     <Login/>
-     <Booking/>
-     <RestrauntList/>
-     <EventHandlerExample/>
-     <EventHandleExampleWithClass/>
+      <Login />
+      <Booking />
+      <RestrauntList />
+      <EventHandlerExample />
+      <EventHandleExampleWithClass />
+      <h1>Below is the basic routing example</h1>
+      <BasicRoutingExample/>
     </div>
   );
 }
